d79733caf96fa756b4f84279527058ff2662abf9
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-24 19:45:38 +0900

    アイコンファイルの削除とアプリ名の変更、レイアウトの改善

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <main className="container">
-      <p>Test</p>
-      <button onClick={() => invoke("test")}>Test Tauri API</button>
-    </main>
+    <>
+      <main className="container">
+        <div className="left-menu">
+          <h1>Menu</h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="page-content">
+          <MainPage />
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,32 @@
+// import React, { useState } from "react";
+// import { invoke } from "@tauri-apps/api/core";
+import "./MainPage.css";
+
+function MainPage() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test...</p>
+      <p>test ...</p>
+      <p>test...</p>
+    </div>
+  );
+}
+
+export default MainPage;
