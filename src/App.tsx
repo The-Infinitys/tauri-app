e0b4320e9e9bb1361eb9134014cbc0c4ae4c0612
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-24 23:01:21 +0900

    多言語対応の追加とスタイルの改善

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import MainPage from "./pages/MainPage";
+import { useTranslation } from 'react-i18next';
+import "./i18n/configs";
 
 function App() {
+  const { t } = useTranslation();
   return (
     <>
       <main className="container">
         <div className="left-menu">
-          <h1>Menu</h1>
+          <h1>{t("application.leftMenu.title")}</h1>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>{t("application.leftMenu.home")}</li>
+            <li>{t("application.leftMenu.search")}</li>
+            <li>{t("application.leftMenu.settings")}</li>
           </ul>
         </div>
         <div className="page-content">
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,30 +1,15 @@
 // import React, { useState } from "react";
 // import { invoke } from "@tauri-apps/api/core";
 import "./MainPage.css";
+import { useTranslation } from "react-i18next";
 
 function MainPage() {
+  const { t } = useTranslation();
   return (
     <div>
-      <h1>Welcome!</h1>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test...</p>
-      <p>test ...</p>
-      <p>test...</p>
+      <h1>{t("application.pages.home.title")}</h1>
+      <p>{t("application.pages.home.descriptions.first")}</p>
+      <p>{t("application.pages.home.descriptions.second")}</p>
     </div>
   );
 }
